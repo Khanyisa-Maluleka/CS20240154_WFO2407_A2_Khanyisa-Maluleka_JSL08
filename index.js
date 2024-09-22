3cf4d4de3c214a85efbc544365e51a8e24cb8cb1
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-09-22 11:25:25 +0200

    created my return function

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class BankBranch {
             bankBranchInstance = this;
         }
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
+        return bankBranchInstance;
     }
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 }
